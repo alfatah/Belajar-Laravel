d05a46c8ab36191e58dce243839dfa78f5520f3f
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-10-22 14:15:21 +0700

    update

--- laravel/resources/views/category.blade.php
+++ laravel/resources/views/category.blade.php
@@ -7,7 +7,7 @@
     @foreach ( $posts as $post )
     <article>
         <h2>
-            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            <a href="/posts/{{ $post->slug }}">{{ $post->title] }}</a>
         </h2>
         <h5>{{ $post->author }}</h5>
         <p>{!! $post->excerpt !!}</p>
@@ -15,4 +15,3 @@
     @endforeach
 
 @endsection
-
