89f0d6f3fbf4805ae98052c02430cbf8ca7f9589
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-10-26 13:00:02 +0700

    update

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -22,7 +22,29 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+            Post::create[
+                'name' => 'Sholahuddin Alfatah',
+                'email' => 'alfatah1505@gmail.com',
+                'password' => bcrypt('12345')
+            ]);
 
+            Category::create([
+                'name' => 'Web Programming',
+                'slug' => 'web-programming'
+            ]);
+
+            Category::create([
+                'name' => 'Personal',
+                'slug' => 'personal'
+            ]);
+
+            Post::create([
+                'title' => 'Judul Pertama',
+                'slug' => 'personal',
+                'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, placeat ex voluptas ad perferendis veniam explicabo, nesciunt ratione vero voluptates velit labore cum dignissimos. Dolor quia cum et nam beatae quam, sunt voluptatibus sequi animi fuga non pariatur numquam suscipit nostrum sit quos. Ab nulla, ipsum amet dicta, quibusdam possimus facere tempora laudantium fugiat, aliquam reiciendis neque voluptatibus iste at. Harum voluptates dolores earum magnam ipsam? Laboriosam incidunt iste perspiciatis quos odio accusamus architecto recusandae exercitationem sequi dolorum animi enim blanditiis qui totam, quod eum iure magnam expedita earum nam nostrum nesciunt nisi. Molestias velit sint tenetur deleniti maiores magnam!'
+                'category_id' => 1,
+            ]);
 
 
     }
