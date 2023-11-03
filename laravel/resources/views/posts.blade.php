3afec307c7aeb74df5e475c4b926145ff097b256
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-03 22:29:54 +0700

    update

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 @if ($posts->count())
 
     <div class="card mb-3">
-        <img src="https://source.unsplash.com/random/?{{ $posts[0]->category->name }}" style="width:1100;height:600px; class="card-img-top">
+        <img src="https://source.unsplash.com/random/?{{ $posts[0]->category->name }}" style="width:1100;height:600px; class="card-img-top" alt="{{ $posts[0]->category->name }}">
         <div class="card-body text-center">
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->title }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
 
@@ -27,9 +27,33 @@
 
 @endif
 
+      <div class="container">
+        <div class="row">
+            @foreach($posts->skip(1) as $post)
+
+
+         <div class="col-md-4">
+            <div class="card">
+                <img src="https://source.unsplash.com/random/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }} style="width:400;height:500px;">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $title }}</h5>
+                  <small class="text-muted">
+                    By. <a class="text-decoration-none" href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
+                    {{ $post->created_at->diffForHumans() }}
+                    </small>
+                  <p class="card-text">{!! $post->excerpt !!}</p>
+                  <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                </div>
+              </div>
+
+        </div>
+        @endforeach
+        </div>
+      </div>
+
     <h1 class="mb-5">{{ $title }}</h1>
 
-    @foreach ( $posts as $post )
+    @foreach ( $posts->skip(1) as $post )
     <article class="mb-5 border-bottom pb-4">
         <h2>
             <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
