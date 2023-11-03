db9d1e5c915436e7216587a4d0d62f8bd92d1555
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-03 22:41:19 +0700

    update

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 @if ($posts->count())
 
     <div class="card mb-3">
-        <img src="https://source.unsplash.com/random/?{{ $posts[0]->category->name }}" style="width:1100;height:600px; class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        <img src="https://source.unsplash.com/random/?{{ $posts[0]->category->name }}" style="width:1100;height:600px;" class="card-img-top" alt="{{ $posts[0]->category->name }}">
         <div class="card-body text-center">
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->title }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
 
@@ -34,7 +34,7 @@
 
          <div class="col-md-4">
             <div class="card">
-                <img src="https://source.unsplash.com/random/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }} style="width:400;height:500px;">
+                <img src="https://source.unsplash.com/random/?{{ $post->category->name }}" style="width:250;height:250px;" class="card-img-top" alt="{{ $post->category->name }} ">
                 <div class="card-body">
                   <h5 class="card-title">{{ $title }}</h5>
                   <small class="text-muted">
