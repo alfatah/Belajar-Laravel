d3ef3de0a9c1b620e00d4ee20fd91b04b1df49b1
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-03 23:25:17 +0700

    update

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 @if ($posts->count())
 
     <div class="card mb-3">
-        <img src="https://source.unsplash.com/random/?{{ $posts[0]->category->name }}" style="width:1100;height:600px;" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        <img src="https://source.unsplash.com/random/{{ $posts[0]->category->name }}" style="width:1100;height:600px;" class="card-img-top" alt="{{ $posts[0]->category->name }}">
         <div class="card-body text-center">
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->title }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
 
@@ -32,11 +32,11 @@
             @foreach($posts->skip(1) as $post)
 
 
-         <div class="col-md-4">
+         <div class="col-md-4 mb-3">
             <div class="card">
                 <img src="https://source.unsplash.com/random/?{{ $post->category->name }}" style="width:250;height:250px;" class="card-img-top" alt="{{ $post->category->name }} ">
                 <div class="card-body">
-                  <h5 class="card-title">{{ $title }}</h5>
+                  <h5 class="card-title">{{ $post->title }}</h5>
                   <small class="text-muted">
                     By. <a class="text-decoration-none" href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
                     {{ $post->created_at->diffForHumans() }}
