45598c171ee21de150f5b571e5bdd0746705c840
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-04 23:11:17 +0700

    update

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -6,13 +6,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1 class="mb-5">{{ $post->title }}</h1>
+            <h1 class="mb-5 ">{{ $post->title }}</h1>
 
-            <p>By. Sholahuddin Alfatah in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+            <p>By. Sholahuddin Alfatah in  <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
             {!! $post->body !!}
 
-            <a href="/posts">Back to Posts</a>
+            <a href="/posts" class="text-decoration-none">Back to Posts</a>
 
 
         </div>
