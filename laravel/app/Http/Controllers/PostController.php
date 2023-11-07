86628ba012aaf57074d93e94c3969bbfc8c918ff
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-07 16:49:43 +0700

    update

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -10,17 +10,10 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
-
-        if(request('search')) {
-            $posts->where('title','like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
-
         return view('posts', [
             "title" => "ALL Posts",
             "active" => 'posts',
-            "posts" => $posts->get()
+            "posts" => Post::latest()->filter()->get()
         ]);
     }
 
