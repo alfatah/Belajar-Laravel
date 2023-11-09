39ebb0cb20d6df9c76fac97a047a8cd7da6bdad4
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-09 12:35:14 +0700

    update

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -9,6 +9,9 @@
 <div class="row justify-content-center mb-3">
     <div class="col-md-6">
         <form action="/posts">
+            @if (request('category'))
+                <input type="hidden" name="category" value="{{ request('category') }}">
+            @endif
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
                 <button class="btn btn-danger" type="submit">Search</button>
@@ -24,7 +27,7 @@
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->title }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
 
           <small class="text-muted">
-          By. <a class="text-decoration-none" href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in  <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+          By. <a class="text-decoration-none" href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in  <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
           {{ $posts[0]->created_at->diffForHumans() }}
           </small>
 
@@ -42,7 +45,7 @@
 
          <div class="col-md-4 mb-3">
             <div class="card">
-                <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0, 0, 0, 0.7"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a> </div>
+                <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0, 0, 0, 0.7"><a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a> </div>
                 <img src="https://source.unsplash.com/random/1200x700/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }} ">
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
