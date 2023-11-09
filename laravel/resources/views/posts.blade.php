a93beb8841c282d4b37b0e5941994cb72a0e29c0
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-09 14:41:17 +0700

    update

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -9,9 +9,15 @@
 <div class="row justify-content-center mb-3">
     <div class="col-md-6">
         <form action="/posts">
+
             @if (request('category'))
                 <input type="hidden" name="category" value="{{ request('category') }}">
             @endif
+
+            @if (request('author'))
+            <input type="hidden" name="author" value="{{ request('author') }}">
+            @endif
+
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
                 <button class="btn btn-danger" type="submit">Search</button>
@@ -27,7 +33,7 @@
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->title }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
 
           <small class="text-muted">
-          By. <a class="text-decoration-none" href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in  <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+          By. <a class="text-decoration-none" href="/posts?author={{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in  <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
           {{ $posts[0]->created_at->diffForHumans() }}
           </small>
 
@@ -50,7 +56,7 @@
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
                   <small class="text-muted">
-                    By. <a class="text-decoration-none" href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
+                    By. <a class="text-decoration-none" href="/posts?author={{ $post->author->username }}">{{ $post->author->name }}</a>
                     {{ $post->created_at->diffForHumans() }}
                     </small>
                   <p class="card-text">{!! $post->excerpt !!}</p>
@@ -66,6 +72,11 @@
         @else
       <p class="text-center fs-4">No post found.</p>
         @endif
+
+
+<div class="d-flex justify-content-end">
+{{ $posts->links() }}
+</div>
 
 @endsection
 
