33feb6132253978e3c854a39a628bcaad226a89d
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-16 22:54:24 +0700

    update

--- laravel/resources/views/dashboard/layouts/sidebar.blade.php
+++ laravel/resources/views/dashboard/layouts/sidebar.blade.php
@@ -9,13 +9,13 @@
           <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2 text-black" aria-current="page" href="/dashboard">
+                <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="/dashboard">
                   <svg class="bi"><use xlink:href="#house-fill"/></svg>
                   Dashboard
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2 text-black" href="/dashboard/posts">
+                <a class="nav-link {{ Request::is('dashboard/posts') ? 'active' : '' }}" href="/dashboard/posts">
                   <i class="bi bi-file-text-fill"></i>
                   My Posts
                 </a>
