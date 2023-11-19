fbfbbf522f40d3447f11482975b74bb3017ca900
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-19 13:35:22 +0700

    update

--- laravel/resources/views/dashboard/posts/create.blade.php
+++ laravel/resources/views/dashboard/posts/create.blade.php
@@ -13,7 +13,7 @@
             display: none;
         }
      </style>
-     
+
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Create new post</h1>
   </div>
@@ -24,25 +24,40 @@
 
   <div class="mb-3">
     <label for="title" class="form-label">Title</label>
-    <input type="text" class="form-control" id="title" name="title">
+    <input type="text" class="form-control @error('title') is-invalid @enderror"  id="title" name="title" required autofocus value="{{ old('title') }}">
+    @error('title')
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @enderror
   </div>
 
   <div class="mb-3">
     <label for="slug" class="form-label">Slug</label>
-    <input type="text" class="form-control" id="slug" name="slug">
+    <input type="text" class="form-control  @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug') }}">
+    @error('slug')
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @enderror
   </div>
 
   <div class="mb-3">
     <label for="category" class="form-label">Category</label>
     <select class="form-select" name="category_id">
         @foreach ($categories as $category)
-        <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @if(old('category_id') === $category_id)
+                <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+            @else
         @endforeach
       </select>
   </div>
 
   <div class="mb-3">
     <label for="body" class="form-label">Body</label>
+    @error('body')
+    <p class="text-danger">{{ $message }}</p>
+    @enderror
     <input id="body" type="hidden" name="body">
     <trix-editor input="body"></trix-editor>
   </div>
