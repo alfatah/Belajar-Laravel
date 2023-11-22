776dae5d15b1886d5a3ba781b5a8877b84ce0a6b
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-22 14:09:34 +0700

    update

--- laravel/resources/views/dashboard/posts/create.blade.php
+++ laravel/resources/views/dashboard/posts/create.blade.php
@@ -58,7 +58,8 @@
 
   <div class="mb-3">
     <label for="image" class="form-label">Post Image</label>
-    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+    <img class="img-preview img-fluid">
+    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
     @error('image')
     <div class="invalid-feedback">
         {{ $message }}
@@ -96,7 +97,23 @@
     document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault()
     })
+
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
+        imgPreview.style.display = 'block';
+
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
+
 </script>
+
 
 
 @endsection
