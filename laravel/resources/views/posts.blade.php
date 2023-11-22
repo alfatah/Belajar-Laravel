bfcb87d0cb5f6f5afbeede55cd61d6594e0574fe
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-11-22 12:36:29 +0700

    update

--- laravel/resources/views/posts.blade.php
+++ laravel/resources/views/posts.blade.php
@@ -26,7 +26,15 @@
 
 @if ($posts->count())
     <div class="card mb-3">
+        @if ($posts[0]->image)
+        <div style="max-height: 400px;overflow:hidden">
+            <img src="{{ asset('storage/' . $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}"  class="img-fluid card-img-top" >
+        </div>
+        @else
         <img src="https://source.unsplash.com/random/1100x600/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        @endif
+
+
         <div class="card-body text-center">
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->title }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
 
@@ -49,7 +57,11 @@
          <div class="col-md-4 mb-3">
             <div class="card">
                 <div class="position-absolute px-3 py-2 text-white" style="background-color:rgba(0, 0, 0, 0.7"><a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a> </div>
-                <img src="https://source.unsplash.com/random/1200x700/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }} ">
+                    @if ($post->image)
+                        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}"  class="img-fluid card-img-top" >
+                    @else
+                    <img src="https://source.unsplash.com/random/1200x700/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }} ">
+                    @endif
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
                   <small class="text-muted">
